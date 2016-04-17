1d77812ac87d551c575ca1d049861bc627c16da8
kunkuntang <kuntang@163.com>
2016-04-17 11:24:32 +0800

    index has completed

--- index.php
+++ index.php
@@ -74,7 +74,58 @@ include_once 'conn.php';
 		</div>
 		<div class="section2Btn moreBtn">查看更多&gt;&gt;</div>
 	</div>
-	<img src="img/bannerBg.png"/>
+	<div class="section3">
+		<img class="laptop" src="img/section3Bg.png" />
+		<div class="section3_detail">
+			<p>系统简介</p>
+			<span>随着网络的发展，互联网已经走入了我国大部分人的生活。但是现代教育太过形式化，许多学生学不到自己想学的东西，还有一些人根本没有走入课堂的机会。</br>
+本站可以解决大部分学生的问题，一切资源免费对大家开发，提供不同科目的不同资料，使有知识的人可以把知识传给别人，想学知识的人可以在这里学到知识，让师生有一个交流的平台。
+本网站基于PHP开发的综合网站，让教学资源共享，能够给广大学子，老师提供一个学习交流的平台。</span>
+			<div class="section3Btn moreBtn">了解更多&gt;&gt;</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="footerCon">
+			<div>
+				<span>
+					友情链接
+				</span>
+				<ul>
+					<li>网易</li>
+					<li>QQ</li>
+					<li>新浪</li>
+					<li>谷歌</li>
+					<li>百度</li>
+				</ul>
+			</div>
+			<div>
+				<span>
+					后台留言
+				</span>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+			<div>
+				<span>
+					后台入口
+				</span>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+			<p class="copyright">Copyright &copy;2016G-Try</p>
+		</div>
+		
+	</div>
 	<script type="text/javascript">
 		$('#searchBtn').click(function(){
 			$('#searchBox').toggleClass('searchBtnShow');
